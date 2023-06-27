99aee63fd3cb26cba1844f54c64b8f4aa487f72e
wangning <575583692@qq.com>
2023-06-27 19:41:29 +0800

    feat: 添加网站description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
+        { hid: 'description', name: 'description', content: '海致科技集团' },
         { name: 'format-detection', content: 'telephone=no' }
       ],
       link: [
